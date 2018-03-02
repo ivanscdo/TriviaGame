5b6b35c490c014cb8624efda8fd9ed4a557463c8
Ivan Saucedo <ivan.scdo@gmail.com>
2018-03-02 12:19:51 -0600

    mb 302.12:19 move to pc

--- notes/test.js
+++ notes/test.js
@@ -1,18 +1,41 @@
 $(document).ready(function() {
 
-    // var band = "radiohead";
-    // var album = "pablo honey";
-    // var suffix = "&api_key=44d5637b853c0365609a74cf51ba468f&format=json";
+    var prefix = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="
+    var band = "metallica";
+    var album = "pablo honey";
+    var suffix = "&api_key=44d5637b853c0365609a74cf51ba468f&format=json";
+    var formed = /formed in /;
+    var from = /from/;
+    var fromComma = /,/;
     
-    // $.get("https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + band + suffix).then(function(data){
-    //     console.log("last.fm artist:", data.artist.bio.summary);
+    $.get(prefix + band + suffix).then(function(data){
+        // console.log("last.fm artist:", data.artist.bio.summary);
+        // console.log(data);
+        var bio = JSON.stringify(data.artist.bio.content);
+        // console.log(JSON.stringify(data.artist.bio.content));
+        $(".bio").text(bio);
+        console.log( formed.test(bio) )
+        console.log( bio.search(formed) )
+        var startFormed =  bio.search(formed);
+        console.log( bio.substring(startFormed+10, startFormed+10+4) )
+
+        console.log( from.test(bio) )
+        console.log( bio.search(from) )
+        var startFrom =  bio.search(from);
+        console.log( bio.substring(startFrom+5, startFrom+5+20) )
+        var fromCommaStr = bio.substring(startFrom+5, startFrom+5+20)
+        console.log( fromCommaStr.search(fromComma) )
+        var endFromComma = fromCommaStr.search(fromComma)
+        console.log( fromCommaStr.substring(0, endFromComma) )
+
+    });
+
+    
+    
+    // $.get("https://ws.audioscrobbler.com/2.0/?method=album.search&album=" + album + suffix).then(function(data){
+    //     console.log("last.fm album:", data.results.albummatches.album);
     
     // });
-    
-    // // $.get("https://ws.audioscrobbler.com/2.0/?method=album.search&album=" + album + suffix).then(function(data){
-    // //     console.log("last.fm album:", data.results.albummatches.album);
-    
-    // // });
     
     
     
